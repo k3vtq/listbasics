89f79536f99b13cd6557907418ad2d9a020a677c
Kevin Taylor <taylokev001@warren.k12.in.us>
2024-12-03 10:17:57 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#SECTION #1
+color_list = ["red", "green", "blue", "yellow"]
+
+print(color_list[0])
+print(color_list[-1])
+slice_list = color_list[1:-1]
+print(slice_list)
+
+#SECTION #2
+num_list = [4, 6, 4, 2, 9, 4, 1]
+
+num_list.append(5)
+print(num_list)
+num_list.remove(4)
+print(num_list)
+print(num_list.count(4))
+print(num_list.index(9))
+
+#SECTION #3
+fruit_list = ["apple", "banana", "cherry"]
+fruit_list[1] = "orange"
+print(fruit_list)
+fruit_list.pop()
+print(fruit_list)
